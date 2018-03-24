e1a4b0dc436a179281784e87694b33d93b4ca8ca
ccojocea <ccojocea@gmail.com>
2018-03-24 20:26:55 +0200

    Add some artwork

--- app/src/main/java/com/example/ccojo/udacitymusicalstructureapp/Song.java
+++ app/src/main/java/com/example/ccojo/udacitymusicalstructureapp/Song.java
@@ -14,21 +14,67 @@ public class Song {
     private int songLength;
 
     public Song (String nm){
-       this(nm, "Unknown artist", "Unknown album", 0);
+       this(nm, "Unknown artist", "Unknown album", 0, 0);
     }
 
     public Song (String nm, String art){
-        this(nm, art, "Unknown album", 0);
+        this(nm, art, "Unknown album", 0, 0);
     }
 
     public Song (String nm, String art, String alb){
-        this(nm, art, alb, 0);
+        this(nm, art, alb, 0, 0);
     }
 
-    public Song (String nm, String art, String alb, int imgId){
+    public Song (String nm, String art, int imgId){
+        this(nm, art, "Unknown album", imgId, 0);
+    }
+
+    public Song (String nm, String art, String alb, int songlen){
+        this(nm, art, alb, 0, songlen);
+    }
+
+    public Song (String nm, String art, int imgId, int songlen){
+        this(nm, art, "Unknown album", imgId, songlen);
+    }
+
+    public Song (String nm, int imgId, int songlen){
+        this(nm, "Unknown artist", "Unknown album", imgId, songlen);
+    }
+
+    public Song (String nm, String art, String alb, int imgId, int songlen){
         songName = nm;
         artistName = art;
         albumName = alb;
-        songImageId = imgId;
+        if(imgId == 0){
+            songImageId = R.drawable.musical_note512w1;
+        } else {
+            songImageId = imgId;
+        }
+        if(songlen == 0){
+            //default value for now, until this can be taken out of file
+            songLength = 300;
+        } else {
+            songLength = songlen;
+        }
+    }
+
+    public int getSongImageId() {
+        return songImageId;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getSongName() {
+        return songName;
     }
 }
--- app/src/main/java/com/example/ccojo/udacitymusicalstructureapp/SongAdapter.java
+++ app/src/main/java/com/example/ccojo/udacitymusicalstructureapp/SongAdapter.java
@@ -3,9 +3,12 @@ package com.example.ccojo.udacitymusicalstructureapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -22,6 +25,35 @@ public class SongAdapter extends ArrayAdapter<Song> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        Song currentSong = getItem(position);
+
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.song_item, parent, false);
+        }
+
+        TextView songNameTV = listItemView.findViewById(R.id.songNameTextView);
+        TextView songArtistTV = listItemView.findViewById(R.id.songArtistTextView);
+        TextView songAlbumTV = listItemView.findViewById(R.id.songAlbumTextView);
+        TextView songLength = listItemView.findViewById(R.id.songLengthTextView);
+
+        songNameTV.setText(currentSong.getSongName());
+        songArtistTV.setText(currentSong.getArtistName());
+        songAlbumTV.setText(currentSong.getAlbumName());
+
+        int seconds = currentSong.getSongLength();
+
+        if(seconds >= 3600){
+            String asTextHours = String.format("%02d:%02d:%02d", seconds/3600, (seconds % 3600)/60, (seconds % 3600)%60);
+            songLength.setText(asTextHours);
+        }else if(seconds >= 60){
+            String asTextMinutes = String.format("%02d:%02d", seconds/60, seconds % 60);
+            songLength.setText(asTextMinutes);
+        } else {
+            final String asTextSeconds = String.format("%d", seconds);
+            songLength.setText("00:" + asTextSeconds);
+        }
+
+        return listItemView;
     }
 }
